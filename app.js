be814d4ebc4f542fed5db72a1687ae76782804d7
fonsecadh <UO258318@uniovi.es>
2020-03-24 18:47:43 +0100

    SDI-NodeJS-8.3-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true}));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
+
 app.use(express.static('public'));
 
 // Variables
@@ -15,8 +18,8 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:rAgHwOlF79pep@tiendamusica-shard-00-00-mmnxq.mongodb.net:27017,tiendamusica-shard-00-01-mmnxq.mongodb.net:27017,tiendamusica-shard-00-02-mmnxq.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 // Rutas / Controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 
 // Lanzar el servidor
