9f46c753c5c33569b0711ace63ae7ab51f3afaa4
fonsecadh <UO258318@uniovi.es>
2020-03-31 21:35:29 +0200

    SDI-NodeJS-9.5-Sistema de compras.

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ routerUsuarioSession.use(function(req, res, next) {
 app.use("/canciones/agregar", routerUsuarioSession);
 app.use("/publicaciones", routerUsuarioSession);
 app.use("/comentarios/:cancion_id", routerUsuarioSession);
+app.use("/cancion/comprar", routerUsuarioSession);
+app.use("/compras", routerUsuarioSession);
 
 // routerUsuarioAutor
 let routerUsuarioAutor = express.Router(); 
@@ -72,7 +74,17 @@ routerAudios.use(function(req, res, next) {
         if (req.session.usuario && canciones[0].autor == req.session.usuario) { 
             next();
         } else { 
-            res.redirect("/tienda");
+            let criterio = { 
+                usuario : req.session.usuario, 
+                cancionId : mongo.ObjectID(idCancion) 
+            };
+            gestorBD.obtenerCompras(criterio, function(compras) { 
+                if (compras != null && compras.length > 0) { 
+                    next();
+                } else { 
+                    res.redirect("/tienda");
+                }
+            });
         }
     })
 });
